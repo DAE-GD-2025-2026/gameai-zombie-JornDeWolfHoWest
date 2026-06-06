97ade9b88233cc067d8cfbf830841609a0045256
Jorn De Wolf (Laptop) <jorn.de.wolf@student.howest.be>
2026-06-06 18:43:15 +0200

    Added BlackBoard & BehaviourTree to be used

---
+++ Plugins/GameFeatures/DeWolfJornZombie/Source/DeWolfJornZombieRuntime/Private/StudentPerceptorDeWolfJorn.cpp
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StudentPerceptorDeWolfJorn.h"
+
+#include "Elements/Framework/TypedElementSorter.h"
+
+
+UStudentPerceptorDeWolfJorn::UStudentPerceptorDeWolfJorn()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStudentPerceptorDeWolfJorn::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    SurvivorPawn = Cast<ASurvivorPawn>(GetOwner());
+
+    if (auto PerceptionComp =
+        GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+    {
+        PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(
+            this,
+            &UStudentPerceptorDeWolfJorn::OnPerceptionUpdated);
+    }
+}
+static AActor* GetClosest(const TArray<AActor*>& List, const FVector& Origin)
+{
+    AActor* Best = nullptr;
+    float BestDist = FLT_MAX;
+
+    for (AActor* A : List)
+    {
+        if (!A) continue;
+
+        float Dist = FVector::Dist(A->GetActorLocation(), Origin);
+
+        if (Dist < BestDist)
+        {
+            BestDist = Dist;
+            Best = A;
+        }
+    }
+
+    return Best;
+}
+
+void UStudentPerceptorDeWolfJorn::TickComponent(
+    float DeltaTime,
+    ELevelTick TickType,
+    FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (!SurvivorPawn)
+        return;
+
+    AAIController* AIController =
+        Cast<AAIController>(SurvivorPawn->GetController());
+
+    if (!AIController)
+        return;
+
+    UBlackboardComponent* BB = AIController->GetBlackboardComponent();
+
+    if (!BB)
+        return;
+
+    FVector Origin = SurvivorPawn->GetActorLocation();
+
+    // 1. ZOMBIE PRIORITY
+    AActor* Zombie = GetClosest(VisibleZombies, Origin);
+    BB->SetValueAsObject(TEXT("ZombieTarget"), Zombie);
+
+    // 2. ITEM PRIORITY
+    AActor* Item = GetClosest(VisibleItems, Origin);
+    BB->SetValueAsObject(TEXT("ItemTarget"), Item);
+
+    // 3. HOUSE PRIORITY
+    AActor* House = GetClosest(VisibleHouses, Origin);
+    BB->SetValueAsObject(TEXT("HouseTarget"), House);
+
+    // 4. STATE DECISION (simple logic for now)
+    if (Zombie)
+    {
+        BB->SetValueAsName(TEXT("CurrentState"), TEXT("FLEE"));
+    }
+    else if (Item)
+    {
+        BB->SetValueAsName(TEXT("CurrentState"), TEXT("LOOT"));
+    }
+    else
+    {
+        BB->SetValueAsName(TEXT("CurrentState"), TEXT("EXPLORE"));
+    }
+}
---
+++ Plugins/GameFeatures/DeWolfJornZombie/Source/DeWolfJornZombieRuntime/Public/StudentPerceptorDeWolfJorn.h
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Damage.h"
+#include "Perception/AISense_Damage.h"
+#include "GameAI_Zombie/Survivor/SurvivorPawn.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "AIController.h"
+
+#include "StudentPerceptorDeWolfJorn.generated.h"
+
+UENUM()
+enum class ESurvivorState : uint8
+{
+	Explore,
+	Loot,
+	Flee
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DEWOLFJORNZOMBIERUNTIME_API UStudentPerceptorDeWolfJorn : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UStudentPerceptorDeWolfJorn();
+
+	virtual void BeginPlay() override;
+
+	virtual void TickComponent(
+		float DeltaTime,
+		ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION()
+	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+private:
+	ASurvivorPawn* SurvivorPawn;
+
+	UPROPERTY()
+	TArray<AActor*> VisibleZombies;
+
+	UPROPERTY()
+	TArray<AActor*> VisibleHouses;
+
+	UPROPERTY()
+	TArray<AActor*> VisibleItems;
+
+	ESurvivorState CurrentState = ESurvivorState::Explore;
+
+public:
+
+	bool HasZombie() const
+	{
+		return VisibleZombies.Num() > 0;
+	}
+
+	bool HasHouse() const
+	{
+		return VisibleHouses.Num() > 0;
+	}
+
+	bool HasItem() const
+	{
+		return VisibleItems.Num() > 0;
+	}
+
+	const TArray<AActor*>& GetVisibleZombies() const
+	{
+		return VisibleZombies;
+	}
+
+	const TArray<AActor*>& GetVisibleHouses() const
+	{
+		return VisibleHouses;
+	}
+
+	const TArray<AActor*>& GetVisibleItems() const
+	{
+		return VisibleItems;
+	}
+};
